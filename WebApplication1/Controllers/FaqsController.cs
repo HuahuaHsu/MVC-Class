dd1fc7a1813321d023a71aeb40357d0829062bc5
ispan_grapefarm <toshirayu@gmail.com>
2026-02-23 15:58:50 +0800

    faqs crud註解

--- WebApplication1/Controllers/FaqsController.cs
+++ WebApplication1/Controllers/FaqsController.cs
@@ -52,14 +52,19 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        // POST: Faqs/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+		//2:53講GET/POST
+		//3:03 Model Binding 自動根據傳來的資訊，解析並把值塞進去
+		//且model binding只針對properties，對於fields是無法綁定的
+
+		// POST: Faqs/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Question,Answer,IsActive,DisplayOrder")] Faq faq)
         {
-            if (ModelState.IsValid)
+			//ModelState.IsValid會檢查Model Binding的過程中，是否有任何錯誤發生，例如資料類型不匹配、必填欄位缺失等，如果有錯誤，ModelState.IsValid會返回false，表示模型狀態無效。
+			if (ModelState.IsValid)
             {
                 _context.Add(faq);
                 await _context.SaveChangesAsync();
@@ -68,8 +73,11 @@ namespace WebApplication1.Controllers
             return View(faq);
         }
 
-        // GET: Faqs/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+		// GET: Faqs/Edit/5
+		// GET是透過網址列傳遞參數，通常用於獲取資料或顯示編輯表單，而POST則是用於提交修改後的資料到伺服器進行處理。
+		// POST，當用戶提交表單時，瀏覽器會將表單數據封裝在HTTP請求的主體中，並通過HTTPS協議將請求發送到伺服器。伺服器接收到請求後，會根據URL中的id參數和表單數據中的faq對象來處理編輯操作。
+
+		public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
             {
@@ -84,14 +92,20 @@ namespace WebApplication1.Controllers
             return View(faq);
         }
 
-        // POST: Faqs/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+		// POST: Faqs/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
+		//Post方法的Edit(int id, [Bind("Id,Question,Answer,IsActive,DisplayOrder")] Faq faq)接受兩個參數：id和faq。id是從URL中傳遞過來的，而faq則是從表單數據中綁定過來的。
+
+
+		[HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Question,Answer,IsActive,DisplayOrder")] Faq faq)
         {
-            if (id != faq.Id)
+
+			//if (id != faq.Id)這行代碼的作用是檢查URL中的id是否與表單數據中的Id一致，如果不一致，則返回NotFound()，表示找不到該資源。這是一種安全措施，可以防止用戶通過修改URL或表單數據來嘗試修改其他資源。
+			if (id != faq.Id)
             {
                 return NotFound();
             }
@@ -137,8 +151,10 @@ namespace WebApplication1.Controllers
             return View(faq);
         }
 
-        // POST: Faqs/Delete/5
-        [HttpPost, ActionName("Delete")]
+		// POST: Faqs/Delete/5
+		// actionName("Delete")的作用是將DeleteConfirmed方法的路由名稱設置為Delete，這樣當用戶訪問Delete路由時，實際上會執行DeleteConfirmed方法。這是一種常見的做法，用於處理刪除操作的POST請求，因為通常我們希望在刪除之前先顯示一個確認頁面，讓用戶確認是否真的要刪除該資源。
+		//避免有兩個Delete方法，分別處理GET和POST請求，這樣可以讓代碼更清晰，並且符合RESTful API的設計原則。
+		[HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
