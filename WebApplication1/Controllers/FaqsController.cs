8f7412e41db42690c5ec1962ca1c0400694a9c68
Hua <b5586164@gmail.com>
2026-02-23 16:14:30 +0800

    使用精靈修改其內容

--- WebApplication1/Controllers/FaqsController.cs
+++ WebApplication1/Controllers/FaqsController.cs
@@ -53,7 +53,7 @@ namespace WebApplication1.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Question,Answer,IsActive,DisplayOrder")] DboFaq dboFaq)
+        public async Task<IActionResult> Create(DboFaq dboFaq)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace WebApplication1.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Question,Answer,IsActive,DisplayOrder")] DboFaq dboFaq)
+        public async Task<IActionResult> Edit(int id,DboFaq dboFaq)
         {
             if (id != dboFaq.Id)
             {
