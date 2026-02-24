03fcbbf120ec3fbe13256e9e6cd54b62e625d168
Xin <liou0710@gmail.com>
2026-02-24 14:57:55 +0800

    Users Model

---
+++ WebApplication3/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication3.Models.EfModels;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Controllers
+{
+	public class UsersController : Controller
+	{
+		private readonly ISpanDemoContext _context;
+
+		public UsersController(ISpanDemoContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			//取得User列表
+			var users = _context.Users
+						.Select(u => new UserItemViewModel { Id = u.Id, UserName = u.UserName })
+						.ToList();
+
+			return View(users);
+		}
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Create(UserCreateViewModel vm )
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new User{UserName = vm.UserName};
+				_context.Users.Add(user);
+				_context.SaveChanges();
+				await _context.SaveChangesAsync();
+
+				//TempData["msg"] = "已新增資料";
+
+				return RedirectToAction(nameof(Index));
+			}
+			
+			return View(vm);
+		}
+
+	}
+}
+
---
+++ WebApplication3/ViewModels/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.ViewModels
+{
+	public class UserItemViewModel
+	{
+		public int Id { get; set; }
+
+		[Display(Name="使用者帳號")]
+		public string UserName { get; set; }
+	}
+
+	public class UserCreateViewModel
+	{
+
+		[Display(Name = "使用者帳號")]
+		[Required]
+		//public int Id { get; set; } // 創建不需要 Id，因為它是由資料庫自動生成的
+		public string UserName { get; set; }
+	}
+
+	public class UserUpdateViewModel
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+	}
+}
