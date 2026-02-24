454664bae85be67c81266aebf8e6b010e3012ab8
ispan_grapefarm <toshirayu@gmail.com>
2026-02-24 14:37:27 +0800

    viewmodel

--- WebApplication2/ViewModels/UserViewModel.cs
+++ WebApplication2/ViewModels/UserViewModel.cs
@@ -1,8 +1,16 @@
-﻿namespace WebApplication2.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.ViewModels
 {
 	public class UserItemViewModel
 	{
+		
+		
 		public int Id { get; set; }
+
+		//dto用來傳輸資料，viewmodel則用來顯示資料
+		//與dto不同，viewmodel還可以有attribute，來定義一些顯示的資訊
+		[Display(Name ="使用者帳號")]
 		public string UserName { get; set; }
 	}
 
