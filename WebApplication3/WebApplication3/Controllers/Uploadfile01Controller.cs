6abae2b55e056e78425e4b7d4c216f94d4c0703a
Hua <b5586164@gmail.com>
2026-02-25 10:36:37 +0800

    檔案上傳的判斷規則寫法(寫在控制器內的方法)

--- WebApplication3/WebApplication3/Controllers/Uploadfile01Controller.cs
+++ WebApplication3/WebApplication3/Controllers/Uploadfile01Controller.cs
@@ -6,6 +6,20 @@ namespace WebApplication3.Controllers
 	{
 		public IActionResult Create(IFormFile f)
 		{
+			var allowExts = new string[] { ".jpg", ".png", "jpeg" };
+			if (f == null || f.Length == 0)
+				ModelState.AddModelError("", "必須上傳檔案");
+			else
+			{
+				string ext = Path.GetExtension(f.FileName); //抓路徑的副檔名".jpg"
+				if (allowExts.Contains(ext) == false)
+					ModelState.AddModelError("", "副檔名不合規定");
+				else
+				{
+
+				}
+			}
+
 			return View();
 		}
 	}
