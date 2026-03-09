e582c4439683f87a11bc7751822204fbb5b24827
Hua <b5586164@gmail.com>
2026-03-09 16:34:45 +0800

    ajax

---
+++ AJAX/WebApplication4/Controllers/AjaxController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+	public class AjaxController : Controller
+	{
+		private readonly NorthwindContext _context;
+
+		public AjaxController(NorthwindContext context)
+		{
+			_context = context;
+		}
+
+		// GET: /Ajax/Greet
+		[HttpGet]
+		public string Greet(string name)
+		{
+			Thread.Sleep(3000); // Simulate a delay
+			return $"Hello, {name}!";
+		}
+
+		// POST: /Ajax/PostGreet
+		[HttpPost]
+		public string PostGreet(string name)
+		{
+			Thread.Sleep(3000);
+			return $"Hello, {name}!";
+		}
+
+		// POST: /Ajax/CheckName
+		[HttpPost]
+		public string CheckName(string firstName)
+		{
+			bool exists = _context.Employees.Any(e => e.FirstName == firstName);
+			return exists ? "true" : "false";
+		}
+	}
+}
---
+++ AJAX/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+		// GET: /Home/Index
+		[HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		//// GET: /Home/Index
+		//[HttpGet]
+		//public IActionResult Index(int n)
+		//{
+		//	return View();
+		//}
+
+
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+		public IActionResult Fetch()
+		{
+			return View();
+		}
+
+		public IActionResult CheckName()
+		{
+			return View();
+		}
+
+
+	}
+}
